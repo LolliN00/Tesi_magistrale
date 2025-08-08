7c422bf6366b31ba60d1d08699640387ee1de528
LolliN00 <lorenzonobili10@gmail.com>
2025-08-08 11:42:55 +0200

    Build Matrix for minimal realization

--- MFD_reduction_algorithm/construct_row_reduced_left_MFD.m
+++ MFD_reduction_algorithm/construct_row_reduced_left_MFD.m
@@ -1,4 +1,4 @@
-function [D_l,N_l,G] = construct_irreducible_left_MFD(H)
+function [D_l,N_l,G] = construct_row_reduced_left_MFD(H)
 [N_inital,~,D_initial] = calculate_lcm_and_initial_MFD(H);
 [D_lc,N_lc,~] = calculate_left_coprime_rapresentation(D_initial,N_inital);
 [U,D_l] = calculate_row_reduced_form(D_lc);
